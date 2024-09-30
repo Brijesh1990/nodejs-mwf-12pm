9e902187a371db5ea511f974e4b6bca699962223
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-09-30 13:27:35 +0530

    30-09-2024 all data uploaded

---
+++ module1-nodejs/what is node js datatype/datatype.js
@@ -0,0 +1,29 @@
+/*  data type : A data type is which type of data or values stored inside of variables i.e called data-type  
+
+   types of data types--
+
+   a)number
+   b)boolean
+   c)string
+   d)bigInteger
+   e)null
+   f)undefined
+   g)Symbol
+   h)object
+*/
+
+
+let a=10; //number
+let b=10.64464; //number
+let c=102121n; //bigInteger
+let d; //undefined
+let e=null; //null
+let f="brijesh"; //string
+let g={id:2,name:"brijesh"} //object
+let h=Symbol(10.56454); //Symbol
+let i=true; //boolean
+let j=false;
+
+// console.log(typeof(a,'\n',b));
+// console.log(typeof(h));
+console.log(typeof(g));
